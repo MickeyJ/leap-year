dfe695999f4613db569e3e02de87081261b6c006
Mickey <develop.agility@gmail.com>
2016-02-12 12:31:09 -0700

    mocha is working

---
+++ main.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  isLeapYear: function(y){
+
+    return true;
+  }
+};
---
+++ test/test.js
@@ -0,0 +1,12 @@
+var code = require('../main.js');
+var expect = require('chai').expect;
+
+describe('should return true only for', function(){
+
+  it('returns true for 2016', function(){
+    expect(code.isLeapYear(2016)).to.equal(true);
+  });
+
+
+
+});
